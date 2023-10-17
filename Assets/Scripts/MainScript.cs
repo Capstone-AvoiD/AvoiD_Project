5840009ee6dd7e5323e16dfdbf5e96f4dd89a9e9
kjeok <kjeok00@gmail.com>
2023-10-17 13:19:25 +0900

    TMP로 텍스트 수정 완

--- Assets/Scripts/MainScript.cs
+++ Assets/Scripts/MainScript.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class MainScript : MonoBehaviour //Panel 활성, 비활성
 {
     private GameObject NPCDialog;
-    private Text NPCText ;
+    private TextMeshProUGUI NPCText ;
 
     // Start is called before the first frame update
     void Start()
     {
         NPCDialog = GameObject.Find("NPCDialog");
-        NPCText = GameObject.Find("NPCText").GetComponent<Text>();
+        NPCText = GameObject.Find("NPCText").GetComponent<TextMeshProUGUI>();
         NPCDialog.SetActive(false);
     }
 
