7e8b6ef932a4589b7ae579bfb618359ff89e6f5b
한인철 <InZury0923@gmail.com>
2023-10-17 17:34:43 +0900

    미니 게임 구성_중간

---
+++ Assets/Scripts/MiniGamePlayer.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniGamePlayer : MonoBehaviour
+{
+
+    private float player_speed = 3.0f;
+    private Vector2 player_direction;
+    private Rigidbody2D player_rigid;
+
+    private void Awake()
+    {
+        player_rigid = gameObject.GetComponent<Rigidbody2D>();
+        player_direction = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Input_Direction();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private Vector2 Input_Direction()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        player_direction = new Vector2(h, v);
+
+        return player_direction;
+    }
+
+    private void Move()
+    {
+        player_rigid.MovePosition(player_rigid.position + player_direction * player_speed * Time.deltaTime);
+        // transform.Translate(player_direction * Time.deltaTime * player_speed);
+    }
+
+    private void OnCollisionStay2D(Collision2D other) 
+    {
+
+    }
+}
