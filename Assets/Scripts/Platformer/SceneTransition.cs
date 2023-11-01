353e707422c57d9c8f3e90defcd620098bdad42d
kjack00 <129487583+kjeok00@users.noreply.github.com>
2023-11-01 18:46:47 +0900

    Goal도착시 Scene 전환

---
+++ Assets/Scripts/Platformer/SceneTransition.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)  //player와 Goal이 접촉하는 경우
+    {
+        if (collision.gameObject.name == "player")
+        {
+            Invoke("LoadMiniGameScene", 2.0f);  //2초 대기 후 미니게임씬으로 전환
+        }
+    }
+    private void LoadMiniGameScene()    //미니게임씬으로 전환
+    {
+        SceneManager.LoadScene("MiniGameScene", LoadSceneMode.Single);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
