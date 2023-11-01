09299e8efd3d7ea49aaa1d4ef6513f66a6745c49
kjack00 <129487583+kjeok00@users.noreply.github.com>
2023-11-01 18:21:13 +0900

    카메라 추적

---
+++ Assets/Scripts/Platformer/CameraFollow.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y + 3, -10f);   //플레이어 위치 추적
+    }
+}
+
