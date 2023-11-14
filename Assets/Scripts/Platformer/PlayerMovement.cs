824b5431607ffb2a1b7ce52e65de8f1ebdeaaf3b
kjack00 <129487583+kjeok00@users.noreply.github.com>
2023-11-15 04:09:30 +0900

    애니메이션(수정필요)
    플랫포머 애니메이션

--- Assets/Scripts/Platformer/PlayerMovement.cs
+++ Assets/Scripts/Platformer/PlayerMovement.cs
@@ -6,10 +6,15 @@ public class PlayerMovement : MonoBehaviour
 {
     float speed = 10.0f;
     public GameObject player;
+    SpriteRenderer spriteRenderer;
+    Rigidbody2D rigid;
+    Animator anim;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,5 +24,15 @@ public class PlayerMovement : MonoBehaviour
         float horizontal_input = Input.GetAxis("Horizontal");
 
         transform.Translate(Vector3.right * horizontal_input * distance_per_frame); //좌,우 방향키로 이동
+
+        //방향 전환
+        if(Input.GetButtonDown("Horizontal"))
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+        
+        //걷기 애니메이션
+        if (Mathf.Abs(rigid.velocity.x) < 0.3)
+            anim.SetBool("isWalkingRight", false);
+        else
+            anim.SetBool("isWalkingRight", true);
     }
 }
