501ce026d273f64d7597e803e7c3ad7989dd7ea2
kjack00 <129487583+kjeok00@users.noreply.github.com>
2023-11-29 18:23:14 +0900

    플랫포머 npc와 대화

--- Assets/Scripts/Platformer/MainScript.cs
+++ Assets/Scripts/Platformer/MainScript.cs
@@ -8,18 +8,32 @@ using System.Text;
 public class MainScript : MonoBehaviour //Panel 활성, 비활성
 {
     private GameObject NPCDialog;
-    private TextMeshProUGUI NPCText ;
+    private GameObject NextText;
+    private TextMeshProUGUI NPCText;
+    private TextMeshProUGUI NPCName;
     public string[] m_text;
     private int currentIndex;
+
+    private string[] playerName = new string[] { "Dohun", "Seongtae", "Sohyun" };
+    private int currentPlayer;  // 도훈: 0, 성태: 1, 소현: 2
+    private GameObject NPC_Dohun;
+    private GameObject NPC_Seongtae;
+    private GameObject NPC_Sohyun;
+
     StringBuilder strb = new StringBuilder();
 
     // Start is called before the first frame update
         
     void Start()
     {
+        DialogImage();
+
         NPCDialog = GameObject.Find("NPCDialog");
+        NextText = GameObject.Find("NextText");
         NPCText = GameObject.Find("NPCText").GetComponent<TextMeshProUGUI>();
+        NPCName = GameObject.Find("NPCName").GetComponent<TextMeshProUGUI>();
         NPCDialog.SetActive(false); //Panel 비활성
+        NextText.SetActive(false); //Panel 비활성
         currentIndex = 0;
     }
 
@@ -34,26 +48,80 @@ public class MainScript : MonoBehaviour //Panel 활성, 비활성
     {
         StopAllCoroutines();    //코루틴 종료
         //StopCoroutine(TypingText());
-        NPCText.text = "";
         NPCDialog.SetActive(false);
+        NPCText.text = "";
+        NPCName.text = "";
         currentIndex = 0;
+        
     }
 
     IEnumerator TypingText()    //텍스트 타이핑 효과 코루틴
     {
         while (currentIndex < m_text.Length)    //모든 지정 텍스트 출력
         {
-            string sentence = m_text[currentIndex];
+            switch (currentPlayer)   //말하는 캐릭터 이미지
+            {
+                case 0:
+                    NPC_Dohun.SetActive(true);
+                    NPC_Seongtae.SetActive(false);
+                    NPC_Sohyun.SetActive(false);
+                    break;
+                case 1:
+                    NPC_Dohun.SetActive(false);
+                    NPC_Seongtae.SetActive(true);
+                    NPC_Sohyun.SetActive(false);
+                    break;
+                case 2:
+                    NPC_Dohun.SetActive(false);
+                    NPC_Seongtae.SetActive(false);
+                    NPC_Sohyun.SetActive(true);
+                    break;
+            }
 
-            yield return new WaitForSeconds(0.5f);  //타이핑 시작 대기
+            switch (currentIndex)   //말하는 캐릭터 이미지
+            {
+                case 0:
+                case 3:
+                    currentPlayer = 0;  //도훈
+                    break;
+                case 1:
+                case 5:
+                    currentPlayer = 1;  //성태
+                    break;
+                case 2:
+                    currentPlayer = 2; // 소현
+                    break;
+            }
+
+            string sentence = m_text[currentIndex];
+            NPCName.text = playerName[0];
+            NextText.SetActive(false);
+
+            //yield return new WaitForSeconds(0.5f);  //타이핑 시작 대기
             for (int i = 0; i <= sentence.Length; i++)
             {
                 NPCText.text = sentence.Substring(0, i);
                 yield return new WaitForSeconds(0.15f); //타이핑 간격
             }
+            NextText.SetActive(true);
+            yield return new WaitUntil(() => Input.GetKey(KeyCode.G));
             currentIndex++;
         }
-        currentIndex = 0;
+        currentIndex = 0;   //대화 종료시 초기화
+    }
+
+    public void DialogImage()
+    {
+        currentPlayer = 0;
+
+        NPC_Dohun = GameObject.Find("NPCImage_Dohun");
+        NPC_Seongtae = GameObject.Find("NPCImage_Seongtae");
+        NPC_Sohyun = GameObject.Find("NPCImage_Sohyun");
+
+        NPC_Dohun.SetActive(false);
+        NPC_Seongtae.SetActive(false);
+        NPC_Sohyun.SetActive(false);
+
     }
 
     void Update()
